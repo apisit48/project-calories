ba9eaedd0a10650d2d06607ed8a36a4d383acc6c
Apisit B <virus_eddy@hotmail.com>
2023-12-10 17:53:09 +0700

    update Guide

--- src/views/GuideView.vue
+++ src/views/GuideView.vue
@@ -42,6 +42,53 @@
     </div>
 
 
+    <h2> 1. Check your health</h2>
+    <p>
+
+        It’s important to consult your healthcare provider and get a physical medical examination before starting an exercise routine.<br>
+
+        This is particularly important for those new to strenuous and vigorous physical activities.<br>
+
+        An early checkup can detect any health problems or conditions that could put you at risk for an injury during exercise.<br>
+
+        It can also help you optimize your workout, making it easier for you and your personal trainer, if you choose to work with one, to understand your limitations and create an exercise plan tailored to your particular needs.<br>
+
+    </p>
+    <h2>2. Make a plan and set realistic goals</h2>
+    <p>
+        Once you decide to start exercising regularly, try to create a plan that includes attainable steps and goals.<br>
+
+        One way to do this is to start with a plan of easy steps to follow. Then you can continue building on it as your fitness level improves.<br>
+
+        For example, if your goal is to finish a 5-kilometer run, you can start by building a plan that includes shorter runs.<br>
+
+        Once you can finish those short runs, increase the distance until you can run the whole 5 kilometers in one session.<br>
+
+        Starting with small achievable goals will increase your chances of success and keep you motivated every step of the way.<br>
+    </p>
+    <h2>3. Make it a habit</h2>
+    <p>
+      Another key component of exercise success is to stick to your routine.<br>
+
+    It seems to be easier for people to maintain an exercise routine in the long term if they make it a habit and do it regularly.<br>
+
+    A review of studiesTrusted Source concluded that replacing an unhealthy behavior with a new healthier habit is an excellent approach to maintaining it in the long term.<br>
+
+    Furthermore, making a schedule or exercising at the same time every day are good ways to sustain your routine and make it last.<br>
+
+    For example, you can make exercise a habit by planning to work out right after work every day or first thing in the morning. It’s important to choose a time that works best for you.<br>
+    </p>
+
+    <h2>
+      Recommended Youtube Channels That Help You Get Stronger
+    </h2>
+
+    <p>
+      BullyJuice: https://www.youtube.com/@BullyJuice<br>
+      TIFF X DAN: https://www.youtube.com/@TIFFxDAN<br>
+      Will Tennyson: https://www.youtube.com/@WillTennyson<br>
+    </p>
+
 </div>
 </template>
 
