67d682aca788a79289fcab0159b8cb4d65481415
mikggi <zomickey@gmail.com>
2023-12-10 16:33:16 +0700

    cssing

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,91 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <body>
+    <header>
+      <h1>Workout Calories for Beginners</h1>
+    </header>
+
+    <section>
+      <h2>About Us</h2>
+      <p>
+        Welcome to Workout Calories for Beginners! We understand that starting a fitness journey can
+        be overwhelming, and that's why we're here to guide you every step of the way. Our mission
+        is to provide you with the tools and information you need to kickstart your workout routine
+        and make informed choices about your nutrition and fitness goals.
+      </p>
+
+      <h2>What We Offer</h2>
+      <p>
+        At Project Calories, we offer a user-friendly platform that caters specifically to
+        beginners. Our website provides:
+      </p>
+      <ul>
+        <li>Informative food and workout knowledge at a click of a button</li>
+        <li>Calorie tracking tools to help you meet your nutritional needs</li>
+        <li>Informative articles on workouts, nutrition, and healthy living</li>
+        <li>A breif summary on how to get started.</li>
+      </ul>
+
+      <h2>Get Started Today</h2>
+      <p>
+        Ready to take the first step towards a healthier you? Sign up for a free account and explore
+        the resources we have to offer. Whether you're aiming to lose weight, build muscle, or
+        simply improve your overall well-being, we've got you covered. Remember, every journey
+        begins with a single step, and we're here to help you along the way.
+      </p>
+    </section>
+
+    <footer>
+      <h3>&copy; 2023 Project Calories. All rights reserved.</h3>
+    </footer>
+  </body>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+html,body,footer {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+
+body {
+  font-family: 'Afacad', sans-serif;
+  box-sizing: border-box;
+  background-color: whitesmoke;
+  color: #495057;
+  font-size: large;
+  overflow: auto;
+}
+
+header {
+  background-color: #212121;
+  color: #ffffff;
+  text-align: center;
+  padding: 1em 0;
+}
+
+section {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+h2 {
+  color: #343a40;
+}
+p {
+  margin-bottom: 25px;
+}
+
+footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-color: #212121;
+  color: whitesmoke;
+  height: 150px;
+  width: 100%;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,40 @@
 </script>
 
 <template>
-  <div class="home-container">
-        <h1> Food</h1>
-        <h1> Exercise</h1>
-  </div>
+<body>
+    <div>
+        <h1>Project Calories</h1>
+        <p>Welcome to your homepage with three layers of green background!</p>
+    </div>
+</body>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .home-container {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+<style scoped>
+ 
+  body {
+            margin: 0;
+            padding: 0;
+            height: 100vh;
+            overflow: hidden;
+            background: linear-gradient(to bottom, #c7ecc7 0%, #038b03c9 50%, #c7ecc7 100%);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            color: #fff;
+            font-family: 'Afacad', sans-serif;
+            font-weight: bold;
+            outline: #4CAF50 solid 10px;
+        }
+
+        h1 {
+            font-size: 3em;
+            text-shadow: 4px 4px 8px rgba(0, 0, 0, 0.5);
+        }
+
+        p {
+            font-size: 1.5em;
+            text-shadow: 4px 4px 8px rgba(0, 0, 0, 0.5);
+        }
+        
 </style>
